de9038b09587b01394390cf5d085df844446957c
Mehfuza Walim <mehfuza@live.com>
2025-07-05 10:42:27 +0530

    Add files via upload

---
+++ Exercise1Task1.py
@@ -0,0 +1,8 @@
+#Program to take two numbers from user and perform basic operations on them
+num1=int(input("Enter the first number: "))
+num2= int(input("Enter the second number: "))
+print("\n")
+print("Addition :", num1+num2)
+print("Subtraction :", num1-num2)
+print("Multiplication:", num1*num2)
+print("Division :", num1/num2)
---
+++ Exercise1Task2.py
@@ -0,0 +1,7 @@
+#Program to take first name and last name from user and display greeting with full name
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+full_name=first_name + " " + last_name 
+print("\n")
+print("Hello, ", full_name, "! Welcom to the Python program.")
+
